3589edc5436778857a4331dd6440d7a8547990a5
migc_dev <migc_dev@protonmail.com>
2023-08-10 17:42:09 +0200

    added fonts and wallpapers

--- .config/qtile/README.md
+++ .config/qtile/README.md
@@ -1,1 +1,4 @@
 # debian12-install
+
+scripts and configs for my debian 12 release
+install-qtile.sh
--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -8,9 +8,6 @@ function run {
 }
 
 #Set your native resolution IF it does not exist in xrandr
-#More info in the script
-#run $HOME/.config/qtile/scripts/set-screen-resolution-in-virtualbox.sh
-
 #Find out your monitor name with xrandr or arandr (save and you get this line)
 #xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
 #xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
