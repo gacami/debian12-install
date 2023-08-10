eb9a351cd5a06b25e06a528c0ea4e8f9230c6ef2
gacami <141941018+gacami@users.noreply.github.com>
2023-08-10 17:16:21 +0200

    Add files via upload

---
+++ install-qtile.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# prerequisites
+sudo apt install -y git pipx
+
+# qtile download and installation
+pipx install "git+https://github.com/qtile/qtile.git"
+
+# add to qtile enviorment the psutil package for widget use
+pipx inject qtile psutil
+pipx ensurepath
+
+# qtile registry for lightdm
+cat > ./temp << "EOF"
+[Desktop Entry]
+Name=Qtile
+Comment=Qtile Session
+Type=Application
+Keywords=wm;tiling
+EOF
+
+sudo cp ./temp /usr/share/xsessions/qtile.desktop;rm ./temp
+u=$USER
+sudo echo "Exec=/home/$u/.local/bin/qtile start" | sudo tee -a /usr/share/xsessions/qtile.desktop
+
+# packages needed after installation (compositor, terminal, text editor, launcher, themes ...)
+sudo apt install -y picom alacritty rofi qalculate-gtk vim geany geany-plugin-treebrowser nitrogen mpv lxappearance arc-theme 
+
+# other utils
+sudo apt install -y udiskie lxpolkit
+
+# copy configuration files
+cp -Rf .config ~/
+
+# copy fonts
+cp -Rf .config ~/
+
+cp -Rf Pictures ~/
+
+# create folders in user directory (eg. Documents,Downloads...)
+xdg-user-dirs-update
+
